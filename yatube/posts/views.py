9b54b37beaf39ff92a108ccbed617824e0996ac5
Даниил Алексеенко <cra1ger@bk.ru>
2022-10-27 10:31:00 +0300

    Коммит перед началом проекта

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
 
 def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
     title = 'Это главная страница проекта Yatube'
     context = {
+        'posts': posts,
         'title': title,
     }
     return render(request, template, context) 
 
 
-def group_posts(request):
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
     template = 'posts/group_list.html'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     title = 'Здесь будет информация о группах проекта Yatube'
     context = {
         'title': title,
+        'posts': posts,
     }
     return render(request, template, context)
